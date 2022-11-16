cb8c66039d9b072e0dbad8133688d3533e1233db
Rémy Ollivier <remy.ollivier64@gmail.com>
2022-11-16 14:41:52 +0100

    Init config

---
+++ src/test/java/fr/project/pokedle/PokedleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.project.pokedle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
