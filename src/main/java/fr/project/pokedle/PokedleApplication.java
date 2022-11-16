c6551ad1e4050302a3c5b365fc3494feb96687de
Rémy Ollivier <remy.ollivier64@gmail.com>
2022-11-16 14:41:52 +0100

    Init config

---
+++ src/main/java/fr/project/pokedle/PokedleApplication.java
@@ -0,0 +1,13 @@
+package fr.project.pokedle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedleApplication.class, args);
+	}
+
+}
