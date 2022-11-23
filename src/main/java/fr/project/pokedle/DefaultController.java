e8ec1176007ac9d5543fa7626b0bf546777b2ec2
Rémy Ollivier <remy.ollivier64@gmail.com>
2022-11-23 12:44:55 +0100

    Change import

--- src/main/java/fr/project/pokedle/DefaultController.java
+++ src/main/java/fr/project/pokedle/DefaultController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class DefaultController {
 
     @GetMapping("/home")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/classic")
+    public String classic() {
+        return "classicGame";
     }
 }
