12356b3639c1e7cd4f0d47b8bb8deffe3d47f2cc
Yufu0 <celio.bueri@gmail.com>
2022-11-24 16:05:57 +0100

    delete default controller and add a dependency

--- src/main/resources/static/js/play_pokemon.js
+++ src/main/resources/static/js/play_pokemon.js
@@ -8,3 +8,30 @@ function tryPokemon() {
         alert("Data: " + data + "\nStatus: " + status);
     });
 }
+
+
+
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    if (input.value === "")
+        filter = "42";
+    else
+        filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].getElementsByTagName("span").item(0).textContent;
+        if (txtValue.toUpperCase().startsWith(filter)) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+$(document).ready(() => {
+    document.getElementById("myDropdown").classList.toggle("show");
+    filterFunction();
+});
