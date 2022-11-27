d23b4bc3f9a1914564038b02b5bb992b428e3413
Rémy Ollivier <remy.ollivier64@gmail.com>
2022-11-27 12:14:53 +0100

    Front: create classic game

--- src/main/java/fr/project/pokedle/controller/GameController.java
+++ src/main/java/fr/project/pokedle/controller/GameController.java
@@ -34,7 +34,7 @@ public class GameController {
 
         model.addAttribute("pokemonList", pokemonList);
 
-        return "play/classic";
+        return "classicGame";
     }
 
     @PostMapping(value = "/play/official_try", produces = MediaType.APPLICATION_JSON_VALUE)
