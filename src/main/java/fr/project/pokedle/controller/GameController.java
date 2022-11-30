24835d9f3de91016ab06346c57c3ca5ba66bdfd2
celio <celio.bueri@gmail.com>
2022-11-30 21:30:15 +0100

    Sphash art game ; request image works !!

--- src/main/java/fr/project/pokedle/controller/GameController.java
+++ src/main/java/fr/project/pokedle/controller/GameController.java
@@ -1,17 +1,16 @@
 package fr.project.pokedle.controller;
 
 import fr.project.pokedle.game.classic_game.ClassicGameManager;
+import fr.project.pokedle.game.classic_game.PlayClassicGame;
 import fr.project.pokedle.game.splashart_game.PlaySplashArtGame;
 import fr.project.pokedle.game.splashart_game.SplashArtGameManager;
-import fr.project.pokedle.service.UserDetailsImpl;
-import fr.project.pokedle.game.classic_game.PlayClassicGame;
 import fr.project.pokedle.persistence.data.Pokemon;
 import fr.project.pokedle.repository.PokemonRepository;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import fr.project.pokedle.service.UserDetailsImpl;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,16 +18,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 @Controller("/play")
@@ -92,10 +87,13 @@ public class GameController {
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         ImageIO.write(splashArtGameManager.getImage(userDetails.getUser()), "png", stream);
+        byte[] imageBytes = stream.toByteArray();
+        byte[] imgBase64 = Base64.encodeBase64(imageBytes);
+
         byte [] data = stream.toByteArray();
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.IMAGE_PNG)
-                .body(data);
+                .body(imgBase64);
     }
 
 }
