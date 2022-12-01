4d69fc3d6c63130e6d1fb904863a821fe59d0766
celio <celio.bueri@gmail.com>
2022-12-01 12:58:56 +0100

    le calcule du score en 1 ligne

--- src/main/java/fr/project/pokedle/game/LeaderBoard.java
+++ src/main/java/fr/project/pokedle/game/LeaderBoard.java
@@ -3,6 +3,8 @@ package fr.project.pokedle.game;
 import fr.project.pokedle.game.classic_game.ClassicGameManager;
 import fr.project.pokedle.persistence.game.classic.ClassicGame;
 import fr.project.pokedle.persistence.game.classic.ClassicGamePlayer;
+import fr.project.pokedle.persistence.registration.User;
+import fr.project.pokedle.repository.ClassicGameRepository;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,16 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class LeaderBoard {
     @Autowired
     private ClassicGameManager classicGameManager;
+
+    @Autowired
+    private ClassicGameRepository classicGameRepository;
 
     public JSONObject getScoreOnePlayerToday(ClassicGamePlayer classicGamePlayer) {
         JSONObject json = new JSONObject();
@@ -33,6 +40,29 @@ public class LeaderBoard {
                 .sorted((o1, o2) -> (int) (o2.getScore() - o1.getScore()))
                 .map(this::getScoreOnePlayerToday)
                 .map(jsonScores::add);
+
+        return jsonScores;
+    }
+
+    public JSONArray getLeaderBoardClassicGame(Date date) {
+        JSONArray jsonScores = new JSONArray();
+
+        List<ClassicGame> listGames = classicGameRepository.findAll();
+        Map<User, Double> mapScore = listGames
+                .stream()
+                .map(ClassicGame::getGamePlayers)
+                .flatMap(List::stream)
+                .filter(ClassicGamePlayer::isSuccess)
+                .collect(Collectors.groupingBy(ClassicGamePlayer::getUser))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                list -> list.getValue()
+                                        .stream()
+                                        .reduce(0.0, (sum, classicGamePlayer) -> sum + classicGamePlayer.getScore(), Double::sum)
+                        )
+                );
 
         return jsonScores;
     }
