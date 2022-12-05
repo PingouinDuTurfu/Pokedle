1a6adada57838071029da236572492fdd2158b69
Yufu0 <celio.bueri@gmail.com>
2022-12-05 13:51:17 +0100

    fix getrounds when first round

--- src/main/java/fr/project/pokedle/game/classic_game/ClassicGameManager.java
+++ src/main/java/fr/project/pokedle/game/classic_game/ClassicGameManager.java
@@ -96,19 +96,18 @@ public class ClassicGameManager {
         List<ClassicRound> rounds = getPreviousRounds(user);
         JSONArray json = new JSONArray();
 
-        if (rounds.size() == 0)
-            return json;
+        if (rounds != null && rounds.size() > 0) {
+            Pokemon pokemonToFind = rounds.get(0).getGamePlayer().getGame().getPokemon();
+            rounds.forEach(round -> {
+                ClassicGameTry classicGameTry = new ClassicGameTry(round.getPokemon(), pokemonToFind);
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("is_same", classicGameTry.isSame());
+                jsonObject.put("pokemon", round.getPokemon().toJSON());
+                jsonObject.put("difference", classicGameTry.toJSON());
 
-        Pokemon pokemonToFind = rounds.get(0).getGamePlayer().getGame().getPokemon();
-        rounds.forEach(round -> {
-            ClassicGameTry classicGameTry = new ClassicGameTry(round.getPokemon(), pokemonToFind);
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("is_same", classicGameTry.isSame());
-            jsonObject.put("pokemon", round.getPokemon().toJSON());
-            jsonObject.put("difference", classicGameTry.toJSON());
-
-            json.add(jsonObject);
-        });
+                json.add(jsonObject);
+            });
+        }
         return json;
     }
 }
