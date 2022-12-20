a8dc4efc309991683c0afaf91a150b5bbcdf162e
celio <celio.bueri@gmail.com>
2022-12-20 14:43:54 +0100

    viration du repository

--- src/main/java/fr/project/pokedle/persistence/game/Game.java
+++ src/main/java/fr/project/pokedle/persistence/game/Game.java
@@ -1,7 +1,7 @@
-package fr.project.pokedle.persistence.game.classic;
+package fr.project.pokedle.persistence.game;
 
 import fr.project.pokedle.persistence.data.Pokemon;
-import fr.project.pokedle.persistence.game.Game;
+import fr.project.pokedle.persistence.game.classic.ClassicGamePlayer;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
@@ -13,23 +13,22 @@ import java.util.List;
 import java.util.UUID;
 
 @Setter @Getter
-@Entity(name = "ClassicGames")
-public class ClassicGame extends Game {
+public abstract class Game {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(updatable = false, nullable = false, columnDefinition = "VARCHAR(36)")
     @Type(type = "uuid-char")
-    private UUID id;
+    protected UUID id;
 
     @Column
     @Temporal(TemporalType.DATE)
-    private Date date;
+    protected Date date;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(nullable = false)
-    private Pokemon pokemon;
+    protected Pokemon pokemon;
 
     @OneToMany(mappedBy = "game", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<ClassicGamePlayer> gamePlayers;
+    protected List<ClassicGamePlayer> gamePlayers;
 }
