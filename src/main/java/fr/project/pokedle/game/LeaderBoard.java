a968b48c4915679f711c9b5cceca03942d424e6d
Rémy Ollivier <remy.ollivier64@gmail.com>
2022-12-22 17:35:29 +0100

    Add leaderboard for splash art and button on pages

--- src/main/java/fr/project/pokedle/game/LeaderBoard.java
+++ src/main/java/fr/project/pokedle/game/LeaderBoard.java
@@ -1,23 +1,28 @@
 package fr.project.pokedle.game;
 
 import fr.project.pokedle.game.classic_game.ClassicGameManager;
+import fr.project.pokedle.game.splashart_game.SplashArtGameManager;
 import fr.project.pokedle.persistence.game.classic.ClassicGame;
 import fr.project.pokedle.persistence.game.classic.ClassicGamePlayer;
+import fr.project.pokedle.persistence.game.splashart.SplashArtGame;
+import fr.project.pokedle.persistence.game.splashart.SplashArtGamePlayer;
 import fr.project.pokedle.persistence.registration.User;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
 public class LeaderBoard {
     @Autowired
     private ClassicGameManager classicGameManager;
+    @Autowired
+    private SplashArtGameManager splashArtGameManager;
     @Autowired
     private GameManager gameManager;
 
@@ -35,7 +40,7 @@ public class LeaderBoard {
 
     public JSONArray getLeaderBoardClassicGameOfDay(Date date) {
         JSONArray jsonScores = new JSONArray();
-        ClassicGame classicGame = classicGameManager.getClassicGameOfDay(date);
+        ClassicGame classicGame = classicGameManager.getClassicGameOfDayOrCreate(date);
         List<ClassicGamePlayer> playerList = classicGame.getGamePlayers();
 
         playerList.stream()
@@ -48,13 +53,23 @@ public class LeaderBoard {
     }
 
     public List<ClassicGamePlayer> getClassicMapScoreOfDay(Date date) {
-        return classicGameManager
-                .getClassicGameOfDay(date)
+        ClassicGame classicGame = classicGameManager.getClassicGameOfDay(date);
+        return classicGame == null ? new ArrayList<>() : classicGame
+                                                            .getGamePlayers()
+                                                            .stream()
+                                                            .filter(ClassicGamePlayer::isSuccess)
+                                                            .sorted((o1, o2) -> (int) (o2.getScore() - o1.getScore()))
+                                                            .collect(Collectors.toList());
+    }
+
+    public List<SplashArtGamePlayer> getSplashArtMapScoreOfDay(Date date) {
+        SplashArtGame splashArtGame = splashArtGameManager.getSplashArtGameOfDay(date);
+        return splashArtGame == null ? new ArrayList<>() : splashArtGame
                 .getGamePlayers()
                 .stream()
-                    .filter(ClassicGamePlayer::isSuccess)
-                    .sorted((o1, o2) -> (int) (o2.getScore() - o1.getScore()))
-                    .collect(Collectors.toList());
+                .filter(SplashArtGamePlayer::isSuccess)
+                .sorted((o1, o2) -> (int) (o2.getScore() - o1.getScore()))
+                .collect(Collectors.toList());
     }
 /*
     public Map<User, Double> getMapgeneral(Date date) {
